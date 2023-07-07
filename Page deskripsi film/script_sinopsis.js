a085145f23607075a0bdb2c04f115e214edae722
Muhammad Rizki Arta Maulana <123artha12345@gmail.com>
2023-07-08 00:45:07 +0700

    first commit

---
+++ Page deskripsi film/script_sinopsis.js
@@ -0,0 +1,11 @@
+
+const tglLinks = document.querySelectorAll('.tgl-link');
+
+tglLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        tglLinks.forEach((link) => {
+            link.parentNode.parentNode.classList.remove('selected');
+        });
+        link.parentNode.parentNode.classList.add('selected');
+    });
+});
