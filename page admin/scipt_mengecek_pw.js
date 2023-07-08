ba3a1b16f92dfe44b6ba3a05b40504d255ec3cea
Muhammad Rizki Arta Maulana <123artha12345@gmail.com>
2023-07-08 23:08:28 +0700

    update

--- page admin/scipt_mengecek_pw.js
+++ page admin/scipt_mengecek_pw.js
@@ -4,7 +4,7 @@ function validateLogin() {
     var password = document.getElementById("password").value;
 
     if (user === "admin1" && password === "123") {
-        window.location.href = "/Page%20dasboard/dasboard_admin.html";
+        window.location.href = "dasboard_admin.html";
     } else {
         alert("Email or password is incorrect.");
     }
